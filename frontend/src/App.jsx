df71ac21ed9d416c43042d52d63f46b44bbc8ab5
valelectronic <nnanwikevictor@gmail.com>
2024-08-28 23:08:40 +0100

    creating the header section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,13 @@ import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import AboutUs from './pages/AboutUs';
+import Header from './components/Header';
 
 function App() {
   return (
   
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/profile' element={<Profile />} />
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Home() {
   return (
-    <div className='bg-red-600'>Home</div>
+    <div className=''>Home</div>
   )
 }
 
